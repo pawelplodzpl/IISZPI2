04e6ccd05fe3aa1ae4f4e3b7fea0a6de1a6fb443
pavel <pavel@kis248.kis.p.lodz.pl>
2015-03-25 13:02:29 +0100

    Initial Commit

---
+++ VSO2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VSO2
+//
+//  Created by pavel on 25.03.2015.
+//  Copyright (c) 2015 pavel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
